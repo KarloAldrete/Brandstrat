357ccda91781c2fc68c26bc01ecc83039b4aeab9
Karlo <karlo.aldretev1@gmail.com>
2023-12-14 11:10:27 -0800

    Update pf.ts

--- src/components/pf.ts
+++ src/components/pf.ts
@@ -26,7 +26,7 @@ export default async function Profundidad(projectName: string, fileName: string)
             console.log(`Trabajando con el archivo: ${nombreArchivo}`);
             let intentos = 0;
             const maxIntentos = 3;
-            let archivoDescargado = null;
+            let archivoDescargado: Blob | null = null;
 
             while (intentos < maxIntentos && archivoDescargado === null) {
                 const { data: archivo, error } = await supabaseClient
